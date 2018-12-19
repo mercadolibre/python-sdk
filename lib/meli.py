8d8dd0600084b04ac84d3cae067c59247a614827
Carlos Correa <carlos@truehome.com.mx>
2018-12-19 14:38:48 -0600

    Improve and change some dependencies

--- lib/meli.py
+++ lib/meli.py
@@ -1,14 +1,15 @@
- #!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ConfigParser import SafeConfigParser
-from ssl_helper import SSLAdapter
-from urllib import urlencode
 import json
 import os
 import re
-import requests
 import ssl
+from urllib.parse import urlencode
+import requests
+from configparser import SafeConfigParser
+from .ssl_helper import SSLAdapter
+
 
 class Meli(object):
     def __init__(self, client_id, client_secret, access_token=None, refresh_token=None):
@@ -18,9 +19,8 @@ class Meli(object):
         self.refresh_token = refresh_token
         self.expires_in = None
 
-
         parser = SafeConfigParser()
-        parser.read(os.path.dirname(os.path.abspath(__file__))+'/config.ini')
+        parser.read(os.path.dirname(os.path.abspath(__file__)) + '/config.ini')
 
         self._requests = requests.Session()
         try:
@@ -34,15 +34,16 @@ class Meli(object):
         self.AUTH_URL = parser.get('config', 'auth_url')
         self.OAUTH_URL = parser.get('config', 'oauth_url')
 
-    #AUTH METHODS
-    def auth_url(self,redirect_URI):
-        params = {'client_id':self.client_id,'response_type':'code','redirect_uri':redirect_URI}
-        url = self.AUTH_URL  + '/authorization' + '?' + urlencode(params)
+    # AUTH METHODS
+    def auth_url(self, redirect_URI):
+        params = {'client_id': self.client_id, 'response_type': 'code', 'redirect_uri': redirect_URI}
+        url = self.AUTH_URL + '/authorization' + '?' + urlencode(params)
         return url
 
     def authorize(self, code, redirect_URI):
-        params = { 'grant_type' : 'authorization_code', 'client_id' : self.client_id, 'client_secret' : self.client_secret, 'code' : code, 'redirect_uri' : redirect_URI}
-        headers = {'Accept': 'application/json', 'User-Agent':self.SDK_VERSION, 'Content-type':'application/json'}
+        params = {'grant_type': 'authorization_code', 'client_id': self.client_id, 'client_secret': self.client_secret,
+                  'code': code, 'redirect_uri': redirect_URI}
+        headers = {'Accept': 'application/json', 'User-Agent': self.SDK_VERSION, 'Content-type': 'application/json'}
         uri = self.make_path(self.OAUTH_URL)
 
         response = self._requests.post(uri, params=urlencode(params), headers=headers)
@@ -53,7 +54,8 @@ class Meli(object):
             if 'refresh_token' in response_info:
                 self.refresh_token = response_info['refresh_token']
             else:
-                self.refresh_token = '' # offline_access not set up
+                self.refresh_token = ''
+                # offline_access not set up
                 self.expires_in = response_info['expires_in']
 
             return self.access_token
@@ -63,8 +65,9 @@ class Meli(object):
 
     def get_refresh_token(self):
         if self.refresh_token:
-            params = {'grant_type' : 'refresh_token', 'client_id' : self.client_id, 'client_secret' : self.client_secret, 'refresh_token' : self.refresh_token}
-            headers = {'Accept': 'application/json', 'User-Agent':self.SDK_VERSION, 'Content-type':'application/json'}
+            params = {'grant_type': 'refresh_token', 'client_id': self.client_id, 'client_secret': self.client_secret,
+                      'refresh_token': self.refresh_token}
+            headers = {'Accept': 'application/json', 'User-Agent': self.SDK_VERSION, 'Content-type': 'application/json'}
             uri = self.make_path(self.OAUTH_URL)
 
             response = self._requests.post(uri, params=urlencode(params), headers=headers, data=params)
@@ -79,12 +82,12 @@ class Meli(object):
                 # response code isn't a 200; raise an exception
                 response.raise_for_status()
         else:
-            raise Exception, "Offline-Access is not allowed."
+            raise Exception("Offline-Access is not allowed.")
 
     # REQUEST METHODS
     def get(self, path, params=None, extra_headers=None):
         params = params or {}
-        headers = {'Accept': 'application/json', 'User-Agent':self.SDK_VERSION, 'Content-type':'application/json'}
+        headers = {'Accept': 'application/json', 'User-Agent': self.SDK_VERSION, 'Content-type': 'application/json'}
         if extra_headers:
             headers.update(extra_headers)
         uri = self.make_path(path)
@@ -93,7 +96,7 @@ class Meli(object):
 
     def post(self, path, body=None, params=None, extra_headers=None):
         params = params or {}
-        headers = {'Accept': 'application/json', 'User-Agent':self.SDK_VERSION, 'Content-type':'application/json'}
+        headers = {'Accept': 'application/json', 'User-Agent': self.SDK_VERSION, 'Content-type': 'application/json'}
         if extra_headers:
             headers.update(extra_headers)
         uri = self.make_path(path)
@@ -105,7 +108,7 @@ class Meli(object):
 
     def put(self, path, body=None, params=None, extra_headers=None):
         params = params or {}
-        headers = {'Accept': 'application/json', 'User-Agent':self.SDK_VERSION, 'Content-type':'application/json'}
+        headers = {'Accept': 'application/json', 'User-Agent': self.SDK_VERSION, 'Content-type': 'application/json'}
         if extra_headers:
             headers.update(extra_headers)
         uri = self.make_path(path)
@@ -117,7 +120,7 @@ class Meli(object):
 
     def delete(self, path, params=None, extra_headers=None):
         params = params or {}
-        headers = {'Accept': 'application/json', 'User-Agent':self.SDK_VERSION, 'Content-type':'application/json'}
+        headers = {'Accept': 'application/json', 'User-Agent': self.SDK_VERSION, 'Content-type': 'application/json'}
         if extra_headers:
             headers.update(extra_headers)
         uri = self.make_path(path)
@@ -126,7 +129,7 @@ class Meli(object):
 
     def options(self, path, params=None, extra_headers=None):
         params = params or {}
-        headers = {'Accept': 'application/json', 'User-Agent':self.SDK_VERSION, 'Content-type':'application/json'}
+        headers = {'Accept': 'application/json', 'User-Agent': self.SDK_VERSION, 'Content-type': 'application/json'}
         if extra_headers:
             headers.update(extra_headers)
         uri = self.make_path(path)
